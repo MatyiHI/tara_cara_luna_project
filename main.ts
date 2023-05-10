1cccf719a5a130d49cc95f5e66d60b392a84300d
Kreidler, Matyas Edgar <88744380+MatyiHI@users.noreply.github.com>
2023-05-10 11:35:15 -1000

    Restore '5/10/2023, 11:28:00 AM Update main.blocks'

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, ot
     info.changeLifeBy(-1)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    info.changeLifeBy(mySprite2)
+    info.changeLifeBy(-1)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     info.player1.setLife(25)
@@ -10,11 +10,83 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     mySprite2.setPosition(66, 82)
     scene.setBackgroundImage(assets.image`Road`)
     for (let index = 0; index < 30; index++) {
-        mySprite4 = sprites.create(assets.image`Fire_Ball1`, SpriteKind.Projectile)
-        projectile = sprites.createProjectileFromSprite(assets.image`Fire_Ball1`, mySprite, 100, 27)
+        mySprite4 = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 4 4 . . . . . . . 
+            . . . . . . 4 5 5 4 . . . . . . 
+            . . . . . . 2 5 5 2 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Projectile)
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 4 4 . . . . . . . 
+            . . . . . . 4 5 5 4 . . . . . . 
+            . . . . . . 2 5 5 2 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, 100, 27)
         projectile.follow(mySprite2, 20)
-        mySprite3 = sprites.create(assets.image`Bubbles_1`, SpriteKind.Projectile)
-        projectile = sprites.createProjectileFromSprite(assets.image`projectile2`, mySprite2, 100, 27)
+        pause(2000)
+    }
+    for (let index = 0; index < 34; index++) {
+        info.setLife(25)
+        mySprite3 = sprites.create(img`
+            . . . . . . . . b b . . . . . . 
+            . . . . . . . b 9 1 b . . . . . 
+            . . b b . . . b 9 9 b . . . . . 
+            . b 9 1 b . . b b b . . b b b . 
+            . b 3 9 b . b b b b . b 9 9 1 b 
+            . b b b b b 9 9 1 1 b b 3 9 9 b 
+            . . . . b 9 d 9 1 1 b b b b b . 
+            . . . . b 5 3 9 9 9 b . . . . . 
+            . . b b b 5 3 3 d 9 b . . . . . 
+            . b 5 1 b b 5 5 9 b b b b . . . 
+            . b 5 5 b b b b b b 3 9 9 3 . . 
+            . b b b b b b b . b 9 1 1 9 b . 
+            . . . b 5 5 1 b . b 9 1 1 9 b . 
+            . . . b 5 5 5 b . b 3 9 9 3 b . 
+            . . . . b b b . . . b b b b . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Projectile)
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . b b . . . . . . 
+            . . . . . . . b 9 1 b . . . . . 
+            . . b b . . . b 9 9 b . . . . . 
+            . b 9 1 b . . b b b . . b b b . 
+            . b 3 9 b . b b b b . b 9 9 1 b 
+            . b b b b b 9 9 1 1 b b 3 9 9 b 
+            . . . . b 9 d 9 1 1 b b b b b . 
+            . . . . b 5 3 9 9 9 b . . . . . 
+            . . b b b 5 3 3 d 9 b . . . . . 
+            . b 5 1 b b 5 5 9 b b b b . . . 
+            . b 5 5 b b b b b b 3 9 9 3 . . 
+            . b b b b b b b . b 9 1 1 9 b . 
+            . . . b 5 5 1 b . b 9 1 1 9 b . 
+            . . . b 5 5 5 b . b 3 9 9 3 b . 
+            . . . . b b b . . . b b b b . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite2, 100, 27)
         projectile.follow(mySprite2, 20)
         pause(2000)
     }
@@ -23,7 +95,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
 let mySprite3: Sprite = null
 let projectile: Sprite = null
 let mySprite4: Sprite = null
-let mySprite2 = 0
+let mySprite2: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(assets.image`Yuki`, SpriteKind.Player)
 effects.starField.startScreenEffect()
@@ -32,9 +104,27 @@ game.setDialogTextColor(15)
 game.showLongText("Press A to start", DialogLayout.Center)
 scene.setBackgroundImage(assets.image`Winter_Tree_Background_Night`)
 game.setDialogTextColor(15)
-mySprite2 = sprites.create(assets.image`enemy`, SpriteKind.Enemy)
+mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . d d d d d . . . . . 
+    . . . . . d d d d d d d . . . . 
+    . . . . d d d d d d d d d . . . 
+    . . . . d d d d d d d d d . . . 
+    . . . . d d f d d d f d d . . . 
+    . . . . d d f d d d f d d . . . 
+    . . . . d d d d d d d d d . . . 
+    . . . . d d d d d d d d d . . . 
+    . . . . d d d d d d d d d . . . 
+    . . . . d d d d d d d d d . . . 
+    `, SpriteKind.Enemy)
 info.setLife(23)
 controller.moveSprite(mySprite, 100, 100)
 mySprite.setPosition(123, 88)
 mySprite.setBounceOnWall(true)
+info.setLife(100)
 mySprite2.follow(mySprite, 10)
