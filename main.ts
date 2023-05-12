a98dfd48bfed3147e949cb7e23f4f5fc077a55dd
Kreidler, Matyas Edgar <88744380+MatyiHI@users.noreply.github.com>
2023-05-12 09:37:56 -1000

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,45 +1,77 @@
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, otherSprite) {
-    info.changeLifeBy(-1)
+namespace SpriteKind {
+    export const enemyprojectile = SpriteKind.create()
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    scene.setBackgroundImage(assets.image`Winter_Tree_Background_Day`)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Projectile)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+    mySprite.sayText("Please restart game after music stops to reduce game breaks")
+    for (let index = 0; index < 9999; index++) {
+        sprites.destroyAllSpritesOfKind(SpriteKind.enemyprojectile)
+        pause(5000)
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(assets.image`What_5`, mySprite, 100, 27)
+})
+statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
+    sprites.destroy(mySprite2)
+    sprites.destroyAllSpritesOfKind(SpriteKind.enemyprojectile)
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
+    game.setGameOverMessage(true, "you WIN")
+    pause(5000)
+})
+statusbars.onZero(StatusBarKind.Health, function (status) {
+    sprites.destroy(mySprite)
+    game.gameOver(false)
+    game.setGameOverMessage(false, "game over")
+})
+sprites.onOverlap(SpriteKind.enemyprojectile, SpriteKind.Player, function (sprite, otherSprite) {
+    statusbar.value += -3
+    sprites.destroy(enemy_projectile, effects.trail, 1000)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    info.changeLifeBy(-1)
+    Statusbar2.value += -3
+    sprites.destroy(projectile, effects.trail, 1000)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    info.player1.setLife(25)
+    statusbar = statusbars.create(20, 4, StatusBarKind.Health)
+    statusbar.value = 500
+    statusbar.setColor(7, 2, 3)
+    statusbar.attachToSprite(mySprite)
+    Statusbar2 = statusbars.create(20, 4, StatusBarKind.EnemyHealth)
+    Statusbar2.value = 1000
+    Statusbar2.attachToSprite(mySprite2)
+    Statusbar2.setColor(7, 2, 3)
     mySprite.setPosition(70, 15)
     mySprite2.setPosition(66, 82)
     scene.setBackgroundImage(assets.image`Road`)
-    for (let index = 0; index < 30; index++) {
-        mySprite4 = sprites.create(assets.image`what_2`, SpriteKind.Projectile)
-        projectile = sprites.createProjectileFromSprite(assets.image`What_3`, mySprite, 100, 27)
-        projectile.follow(mySprite2, 20)
-        pause(2000)
+    for (let index = 0; index < 9999; index++) {
+        enemy_projectile = sprites.createProjectileFromSprite(assets.image`What_4`, mySprite2, 100, 18)
+        enemy_projectile.setKind(SpriteKind.enemyprojectile)
+        enemy_projectile.follow(mySprite)
+        pause(600)
     }
-    for (let index = 0; index < 34; index++) {
-        info.setLife(25)
-        mySprite3 = sprites.create(assets.image`What_4`, SpriteKind.Projectile)
-        projectile = sprites.createProjectileFromSprite(assets.image`What_5`, mySprite2, 100, 27)
-        projectile.follow(mySprite2, 20)
-        pause(2000)
-    }
-    music.play(music.createSong(assets.song`First_Song`), music.PlaybackMode.UntilDone)
 })
-let mySprite3: Sprite = null
+let Statusbar2: StatusBarSprite = null
+let enemy_projectile: Sprite = null
+let statusbar: StatusBarSprite = null
 let projectile: Sprite = null
-let mySprite4: Sprite = null
 let mySprite2: Sprite = null
 let mySprite: Sprite = null
-mySprite = sprites.create(assets.image`Yuki`, SpriteKind.Player)
+mySprite = sprites.create(assets.image`What_3`, SpriteKind.Player)
 effects.starField.startScreenEffect()
 scene.setBackgroundImage(assets.image`Stage`)
 game.setDialogTextColor(15)
 game.showLongText("Press A to start", DialogLayout.Center)
 scene.setBackgroundImage(assets.image`Winter_Tree_Background_Night`)
 game.setDialogTextColor(15)
-mySprite2 = sprites.create(assets.image`what_1`, SpriteKind.Enemy)
-info.setLife(23)
+mySprite2 = sprites.create(assets.image`finalBoss`, SpriteKind.Enemy)
 controller.moveSprite(mySprite, 100, 100)
 mySprite.setPosition(123, 88)
 mySprite.setBounceOnWall(true)
-info.setLife(100)
-mySprite2.follow(mySprite, 10)
+mySprite2.follow(mySprite, 50)
+forever(function () {
+    music.play(music.createSong(assets.song`FinalSong`), music.PlaybackMode.UntilDone)
+})
